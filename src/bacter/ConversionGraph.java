7678ae6c150c6737e21f40ccbf496105971b7c72
arthurKocher <arthur.kocher@gmail.com>
2021-11-09 16:27:06 +0100

    Revert "Merge branch 'master' into CompleteCircularGenome"
    This reverts commit d6f10086472db77fbebdc259f66b0d580210c404, reversing
    changes made to b2efdac9530665f59b22540211a395715b750dc9.

--- src/bacter/ConversionGraph.java
+++ src/bacter/ConversionGraph.java
@@ -978,7 +978,7 @@ public class ConversionGraph extends Tree {
             }
         }.visit(parseTree);
 
-        m_nodes = root.getAllChildNodesAndSelf().toArray(new Node[0]);
+        m_nodes = root.getAllChildNodesAndSelf().toArray(m_nodes);
         nodeCount = m_nodes.length;
         leafNodeCount = root.getAllLeafNodes().size();
 
